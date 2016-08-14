36aae2c2e62baf6f509f4cc8716a23bf44bda101
fanqiuxia <fanqiuxia@unionbigdata.com>
2016-08-14 15:05:11 +0800

    server

---
+++ server/dao/buzzDao.js
@@ -0,0 +1,40 @@
+var mysql=require("../node_modules/mysql");
+var cookieParser=require("../node_modules/cookie-parser");
+exports.loadIndexData=function(req,res){
+    var myconnection=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'buzz'
+    });
+    myconnection.connect();
+//    console.log(req.body.allItem);
+    var sql=" select i_title,i_pictop from t_items limit ? ,7 ";
+    var startNum=parseInt(req.body.allItem);
+    //if(startNum==49){
+    //    startNum=0;
+    //}
+    //console.log(startNum);
+    myconnection.query(sql,[startNum],function(err,data){
+        //console.log(data);
+        //console.log(err);
+        res.send(JSON.stringify(data));
+//        console.log(JSON.stringify(data));
+    });
+    myconnection.end();
+}
+exports.detail=function(req,res){
+    var myconnection=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'buzz'
+    });
+    myconnection.connect();
+    var sql="select * from t_item where i_id=?";
+    myconnection.query(sql,[req.body.itemId],function(err,data){
+        res.render("detail",{myTitle:data});
+    });
+    myconnection.end();
+}
+
